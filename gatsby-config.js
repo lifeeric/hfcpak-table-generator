3c2e51fbe8f6847d19dcc3b33c213a7de6d2fd3b
EricGit <hi@ericgit.me>
2021-12-04 13:10:19 -0500

    [FIXED] the meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `HFC PAK table generator`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `HFC Pak trading strategy generator`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
