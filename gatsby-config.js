9c17e9f0abef24e068271fa3bd7c71e111281ce3
EricGit <hi@ericgit.me>
2021-12-05 01:17:37 -0500

    [FIXED] google anayltyics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `HFC PAK table generator`,
     description: `HFC Pak trading strategy generator`,
-    author: `@gatsbyjs`,
+    author: `https://ericgit.me`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -29,6 +29,25 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // Make sure this plugin is first in the array of plugins
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-G-5GQ3R63BD4", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
