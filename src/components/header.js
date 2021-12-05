073d1b530b1c38b05c8f7cbf20d5424a823abbe9
EricGit <hi@ericgit.me>
2021-12-05 02:31:29 -0500

    [FIXED] the header in mobile

--- src/components/header.js
+++ src/components/header.js
@@ -3,28 +3,16 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#fff`,
-      marginBottom: `2.45rem`,
-      fontFamily: "sans-serif",
-      borderBottom: "1px solid #E8E9ED",
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header className="header__container">
+    <div className="header__header">
+      <h1 className="header__title">
         <Link
           to="/"
           style={{
             color: `#555`,
             textDecoration: `none`,
           }}
+          className="header__link"
         >
           {siteTitle}
         </Link>
