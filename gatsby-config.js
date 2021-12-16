5ff780f6687c3e1b3bd96bb2f0a0d5e7a9feefc2
EricGit <hi@ericgit.me>
2021-12-16 15:06:10 -0500

    [ADDED] the google adsense

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-6044095207956298`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
